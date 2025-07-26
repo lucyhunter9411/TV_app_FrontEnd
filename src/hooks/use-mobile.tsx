57442770305a02c815f3811e6cd1e866f3164de9
LucyHunter <superengineer199885@gmail.com>
2025-07-26 20:24:35 +0900

    Update use-mobile.tsx

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -15,5 +15,14 @@ export function useIsMobile() {
     return () => mql.removeEventListener("change", onChange)
   }, [])
 
+  React.useEffect(() => {
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    const onChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    }
+    mql.addEventListener("change", onChange)
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    return () => mql.removeEventListener("change", onChange)
+  }, [])
   return !!isMobile
 }
